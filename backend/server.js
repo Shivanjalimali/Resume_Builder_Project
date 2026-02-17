b8a759f4aea14d33664e26748885b241f329344c
Shivanjalimali <Shivanjali7266@gmail.com>
2026-02-17 19:51:15 +0530

    Fixed port for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+
 
 // Middleware
 app.use(cors());
