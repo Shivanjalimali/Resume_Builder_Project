783bd01df00439b3f6aa9b338ef03a126647d6ee
Shivanjalimali <Shivanjali7266@gmail.com>
2026-02-19 19:13:23 +0530

    Added Docker support and environment variable handling

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 export const connectDB= async()=>{
-    await mongoose.connect('mongodb+srv://shivanjali7266_db_user:Shivanjali%40416408@cluster0.k5rzglb.mongodb.net/ResumeBuilderDB')
+    await mongoose.connect(process.env.MONGO_URL)
 
     .then(()=>console.log("DB IS CONNECTED"));
     
