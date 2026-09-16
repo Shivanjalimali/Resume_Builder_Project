7762190cdbd4436e2d270a283a70ae868a4dcd06
Shivanjalimali <Shivanjali7266@gmail.com>
2026-09-16 11:54:01 +0530

    later uploadFolder changes

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,25 +1,37 @@
-import multer from 'multer'
+import multer from 'multer';
+import path from 'path';
 
-const storage=multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,'uploads/')
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(process.cwd(), 'backend', 'uploads'));
     },
-    filename:(req,file,cb)=>{
-        cb(null,`${Date.now()}-${file.originalname}`)
-    }
-})
 
-//FILE FILTER
-const fileFilter=(req,file,cb)=>{
-  const allowedTypes=['image/jpeg','image/png','image/jpg']
-  if(allowedTypes.includes(file.mimetype))
-  {
-    cb(null,true);
-  }
-  else{
-    cb(new Error("Only .jpeg, .png, .jpg  are allowed formats"),false)
-  }
-}
-const upload=multer({storage,fileFilter});
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+
+// FILE FILTER
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = [
+        'image/jpeg',
+        'image/png',
+        'image/jpg'
+    ];
+
+    if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(
+            new Error("Only .jpeg, .png, .jpg are allowed formats"),
+            false
+        );
+    }
+};
+
+const upload = multer({
+    storage,
+    fileFilter
+});
 
 export default upload;
